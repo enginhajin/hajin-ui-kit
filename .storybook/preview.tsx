ef2259e8301347371daf8b3d238cb58d2ab4bbd0
hajin <enginhajin@gmail.com>
2024-07-09 00:07:41 +0900

    storybook preview.tsx 수정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import GlobalStyle from '../src/GlobalStyle';
 import type { Preview } from '@storybook/react';
-import { ThemeProvider, useTheme } from '@emotion/react';
+import { ThemeProvider } from '@emotion/react';
 import theme from '../src/theme';
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <Story />
     </ThemeProvider>
   ),
 ];
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
